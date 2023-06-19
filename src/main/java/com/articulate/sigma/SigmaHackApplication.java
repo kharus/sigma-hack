0660470d44e3b20fc3480cff941b8f4bc7410e8c
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2023-06-19 16:38:24 +0800

    Convert app to Spring Shell (#12)

--- src/main/java/com/articulate/sigma/SigmaHackApplication.java
+++ src/main/java/com/articulate/sigma/SigmaHackApplication.java
@@ -1,10 +1,14 @@
 package com.articulate.sigma;
 
+import com.articulate.sigma.trans.SUMOKBtoTPTPKBRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.shell.command.annotation.CommandScan;
+import org.springframework.shell.command.annotation.EnableCommand;
 
 @SpringBootApplication
+@EnableCommand(SUMOKBtoTPTPKBRunner.class)
 public class SigmaHackApplication {
 
     public static void main(String[] args) {
