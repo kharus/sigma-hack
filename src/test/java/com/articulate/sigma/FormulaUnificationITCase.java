6231d068812635940cbcbadb5a52a93be6785201
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2023-06-30 14:17:19 +0800

    Final changes

--- src/test/java/com/articulate/sigma/FormulaUnificationITCase.java
+++ src/test/java/com/articulate/sigma/FormulaUnificationITCase.java
@@ -1,61 +1,33 @@
 package com.articulate.sigma;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-
-import java.io.File;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.ListIterator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-/**
- * Created by sserban on 3/1/15.
- */
-@RunWith(Parameterized.class)
 @Tag("com.articulate.sigma.TopOnly")
 public class FormulaUnificationITCase extends UnitTestBase {
-    private static final String TEST_FILE_NAME = "formula_unification_tests.json";
-
-    @Parameterized.Parameter(value = 0)
-    public String f1Text;
-    @Parameterized.Parameter(value = 1)
-    public String f2Text;
-
-
-    @Parameterized.Parameters
-    public static Collection<Object[]> loadParamteres() {
-
-        File jsonTestFile = new File(UnitTestBase.CONFIG_FILE_DIR, TEST_FILE_NAME);
-        JSONParser parser = new JSONParser();
-        List<Object[]> result = new ArrayList<Object[]>();
-
-        try {
-            Object obj = parser.parse(new FileReader(jsonTestFile.getAbsolutePath()));
-            JSONArray jsonObject = (JSONArray) obj;
-            ListIterator<JSONObject> li = jsonObject.listIterator();
-            while (li.hasNext()) {
-                JSONObject jo = li.next();
-                String f1 = (String) jo.get("f1");
-                String f2 = (String) jo.get("f2");
-                result.add(new Object[]{f1, f2});
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return result;
-    }
 
     @Test
     public void test() {
+        String f1Text = """
+                (=>
+                    (instance ?C WalkingCane)
+                    (hasPurpose ?C
+                       (exists (?W)
+                          (and
+                                (instance ?W Walking)
+                                (instrument ?W ?C)))))""";
+
+        String f2Text = """
+                (=>
+                    (instance ?C WalkingCane)
+                      (hasPurpose ?C
+                      (exists (?W)
+                        (and
+                        (instance ?W Walking)
+                        (instrument ?W ?C)))))""";
+
         Formula f1 = new Formula();
         f1.read(f1Text);
         Formula f2 = new Formula();
