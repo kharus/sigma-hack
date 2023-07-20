4d09e29bf631083244bedf602e3e688e00e9a54e
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2023-07-20 12:34:15 +0800

    Move WordNet init

--- src/main/java/com/articulate/sigma/KBConfigProperties.java
+++ src/main/java/com/articulate/sigma/KBConfigProperties.java
@@ -8,11 +8,31 @@ import java.nio.file.Path;
 public class KBConfigProperties {
     private Path sigmaHome;
 
+    private Path baseDir;
+
+    private Path kbDir;
+
     public Path getSigmaHome() {
         return sigmaHome;
     }
 
     public void setSigmaHome(Path sigmaHome) {
         this.sigmaHome = sigmaHome;
+    }
+
+    public Path getBaseDir() {
+        return baseDir;
+    }
+
+    public void setBaseDir(Path baseDir) {
+        this.baseDir = baseDir;
+    }
+
+    public Path getKbDir() {
+        return kbDir;
+    }
+
+    public void setKbDir(Path kbDir) {
+        this.kbDir = kbDir;
     }
 }
